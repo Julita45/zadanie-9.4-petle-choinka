5dae8777ab29a4525022f2b663e8a60b589b6c4d
Julita Lutostańska <julita.lutostanska@gmail.com>
2017-12-03 16:02:07 +0100

    Change js files

--- fullTree.js
+++ fullTree.js
@@ -1,17 +1,15 @@
 function drawTree(a) {
-	for (i=0; i<=a; i++) {
-	
-	var star='';
-		for (j=0; j<=(a-i); j++) {
-		star += ' ';	
-		}	
-		for (k=0; k<=i*2; k++) {
-		star += '*';
-		}
+	for (i = 0; i < a; i++) {
+		var star='';
+			for (j = 1; j < (a - i); j++) {
+				star += ' ';	
+			}	
+			for (k = 0; k <= i * 2; k++) {
+				star += '*';
+			}
 		console.log(star);
 	}
 }
-
 console.log(drawTree(5));
 console.log(drawTree(8));
 console.log(drawTree(20));
--- halfTree.js
+++ halfTree.js
@@ -1,13 +1,12 @@
 function drawTree(a) {
-	for (i=1; i<=a; i++) {
-	var star='';
-		for (j=1; j<=i; j++) {
-		star += '*';
-		}
-	console.log(star);
+	for (var i = 1; i <= a; i++) {
+		var star = '';
+			for (var j = 1; j <= i; j++) {
+				star += '*';
+			}
+		console.log(star);
 	}
 }
-
 console.log(drawTree(5));
 console.log(drawTree(8));
 console.log(drawTree(20));
